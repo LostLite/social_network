c4cb599b475cfa7cdeb9ba0c5f07c8b142af7c00
KE-William <ke-william@KE-Williams-MacBook-Pro.local>
2019-10-02 20:49:31 +0300

    cleared up the repo

--- api/routes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const PostRoutes = require('./post');
-
-router.use('/posts', PostRoutes);
-
-module.exports = router;
--- api/routes/post.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const PostController = require('../controllers/Post');
-
-router.get('/', PostController.getPosts)
-
-module.exports = router;
